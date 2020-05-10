32864a9d9c1eae213538226e2d35bfb2f07d85fe
Owain Kenway <o.kenway@ucl.ac.uk>
2020-05-10 15:34:51 +0100

    Start comparison code.

--- src/TSVTools.pas
+++ src/TSVTools.pas
@@ -5,15 +5,56 @@ interface
 uses 
    classes, sysutils;
 
+function numericCompare(const str1, str2: string): string;
+
 function readTSV(const filename: string): TList;
 
+function compareTSV(const file1, file2, outputfile: string): boolean;
 
 implementation
 
 var
    tempTlist, tempSplit: TStringList;
    i: longint;
+   tempDbl: double;
 
+{ Internal functions + procedures }
+
+{ Can we compare two strings as numbers? }
+function numericValid(const str1, str2: string): boolean;
+   begin
+      numericValid := TRUE;
+      try
+         tempDbl := StrToFloat(str1);
+         tempDbl := StrToFloat(str2);
+      except
+         numericValid := FALSE;
+      end;
+      
+   end;
+
+{ 
+   Compare two strings:
+   if both are numeric, return the difference.
+   if not and they are the same return the string.
+   otherwise construct a string showing they differ.
+}
+function numericCompare(const str1, str2: string): string;
+   begin
+      numericCompare := str1;
+      if (numericValid(str1,str2)) then
+         begin
+            tempDbl := StrToFloat(str2) - StrToFloat(str1);
+            numericCompare := FloatToStr(tempDbl);
+         end
+      else if not (str1 = str2) then
+         begin
+            numericCompare := str1 + ' << >> ' + str2;
+         end;
+   end;
+
+
+{ External functions + procedures }
 function readTSV(const filename: string): TList;
    begin
       readTSV := TList.Create;
@@ -31,4 +72,10 @@ function readTSV(const filename: string): TList;
 
       tempTlist.Free;
    end;
+
+function compareTSV(const file1, file2, outputfile: string): boolean;
+   begin
+      compareTSV := TRUE;
+   end;
+
 end.
