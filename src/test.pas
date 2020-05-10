e2ecf9c385d1f4b37ea32fb1fb4a35f0f58c85f3
Owain Kenway <o.kenway@ucl.ac.uk>
2020-05-10 16:41:50 +0100

    Clean out test code.

--- src/test.pas
+++ src/test.pas
@@ -1,25 +1,9 @@
 program test;
 
 uses
-   TSVTools, classes, sysutils;
+   classes, sysutils;
 
-var
-   st: TList;
-   tmp: TStringList;
-   fail: boolean;
-   t: double;
-   ts: string;
 
 begin
-   st := readTSV('../input/file1.tsv');
-   writeln(IntToStr(st.Count));
-   writeln(inttostr(TStringList(st[0]).Count));
-   writeln(TStringList(st[0])[0]);
-   writeln(IntToStr(st.Count));
-
-   writeln(numericCompare('test','test'));
-   writeln(numericCompare('1','test'));
-   writeln(numericCompare('test','1'));
-   writeln(numericCompare('1.5','1'));
-
+   WriteLn('Does nothing.');
 end.
