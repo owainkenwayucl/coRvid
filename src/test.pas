e6b112ab55386b29c8d75d88ab49d155c7f16c7f
Owain Kenway <o.kenway@ucl.ac.uk>
2020-05-22 12:00:43 +0100

    Restructure tests.
    Add test output to .gitignore.

--- src/test.pas
+++ src/test.pas
@@ -7,29 +7,63 @@ var
    insim, outsim:  SimulationDetails;
    failed: integer;
 
+{
+   Test that if we read and then write out and then read a file it
+   has the same values.
+}
+function test_readwrite_sim(): boolean;
+   begin
+      insim := readSimulationDetails('tests/input/uk-sim-initial.ini');
+      recordSimulationDetails('tests/output/test.ini', insim);
+      outsim := readSimulationDetails('tests/output/test.ini');
+
+      test_readwrite_sim := (compareSimulationDetails(insim, outsim));
+   end;
+
+{
+   Test that compareSimulationDetails works.
+}
+function test_change_sim(): boolean;
+   begin
+      insim := readSimulationDetails('tests/input/uk-sim-initial.ini');
+      outsim := readSimulationDetails('tests/input/uk-sim-initial.ini');
+      outsim.R := 1.2;
+
+      test_change_sim := not (compareSimulationDetails(insim, outsim));
+   end;
+
+{
+   Handle test pass/fail:
+    * Output to terminal
+    * increment fail counter
+}
+function failadd(test: boolean; current: integer) :integer;
+   begin
+      failadd := current;
+      if not test then 
+         begin
+             failadd := current + 1;
+             Write('F');
+         end
+      else
+         begin
+             Write('.');
+         end;
+   end;
+
+{ 
+   Run test suite.
+}
 begin
    failed := 0;
-   insim := readSimulationDetails('tests/input/uk-sim-initial.ini');
-   recordSimulationDetails('tests/output/test.ini', insim);
-   outsim := readSimulationDetails('tests/output/test.ini');
-   if (compareSimulationDetails(insim, outsim)) then
-      begin
-         WriteLn('Test passed.');
-      end
-   else
-      begin
-         WriteLn('Test Failed.');
-         failed := failed + 1;
-      end;
-   outsim.R := 1.2;
-   if not (compareSimulationDetails(insim, outsim)) then
-      begin
-         WriteLn('Test passed.')
-      end
-   else
-      begin
-         WriteLn('Test Failed.');
-         failed := failed + 1;
-      end;
+
+   WriteLn('Running tests: ');
+
+   failed := failadd(test_readwrite_sim(), failed);
+   failed := failadd(test_change_sim(), failed);
+
+   WriteLn('');
+   WriteLn('Tests Failed: ' + IntToStr(failed));
+
    halt(failed);
 end.
