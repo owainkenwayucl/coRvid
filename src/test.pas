2d5e7bfeb203231549b4faeb23d8abdda1ff1a70
Owain Kenway <o.kenway@ucl.ac.uk>
2020-05-22 11:34:32 +0100

    Continue terrible tests.

--- src/test.pas
+++ src/test.pas
@@ -5,13 +5,31 @@ uses
 
 var
    insim, outsim:  SimulationDetails;
+   failed: integer;
 
 begin
+   failed := 0;
    insim := readSimulationDetails('../input/uk-sim-initial.ini');
    recordSimulationDetails('test.ini', insim);
    outsim := readSimulationDetails('test.ini');
    if (compareSimulationDetails(insim, outsim)) then
-      WriteLn('Tests passed.')
+      begin
+         WriteLn('Test passed.');
+      end
    else
-      WriteLn('Tests Failed.');
+      begin
+         WriteLn('Test Failed.');
+         failed := failed + 1;
+      end;
+   outsim.R := 1.2;
+   if not (compareSimulationDetails(insim, outsim)) then
+      begin
+         WriteLn('Test passed.')
+      end
+   else
+      begin
+         WriteLn('Test Failed.');
+         failed := failed + 1;
+      end;
+   halt(failed);
 end.
